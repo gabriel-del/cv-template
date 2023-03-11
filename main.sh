d5ab9ebe1bf920836c2a74aaa6b3eec3f4a26df3
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-03-11 19:38:59 -0300

    1 step treating tex

--- main.sh
+++ main.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+tmp="$(pwd)/tmp"
+cd "$tmp"
 
-cd tmp
+generate_tex(){
 resume=`ls Resume-*.pdf`
 mkdir -pv html tex
 cd html
 ln -v -s ../$resume resume.pdf
 pdftohtml -s resume.pdf -nomerge
 pandoc resume-html.html -o ../tex/resume.tex
+cd ..
+}
+
+# generate_tex
+
+
+cd "$tmp/tex"
+
+sed '/ - page~/d' resume.tex > a.tex
+sed -i '/\hypertarget{page/d' a.tex
+sed -i '/\includegraphics\[/d' a.tex
+sed -i  '/^/N;s/\n//' a.tex
+sed -i  '/~~ • ~/N;s/\n/ /' a.tex
+sed -i  '/~~ • ~/N;s/\n/ /' a.tex
+sed -i  '/^$/N;s/\n//' a.tex
+sed -i  '/~~ • ~/ s/^/{/g' a.tex
+sed -i  '/~~ • ~/ s/[ ]\?$/}/g' a.tex
+sed -i 's/~~ •[ ]\?~[ ]\?/}{/g' a.tex
+sed -i 's/ (. months)$//g' a.tex
+
