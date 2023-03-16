4a5b9670f10d15a527ac8f351ec6e571494eb83d
Gabriel Del <gabrieldelc.barros@gmail.com>
2023-03-16 01:15:35 -0300

    files generated

--- main.sh
+++ main.sh
@@ -1,32 +1,55 @@
 #!/bin/bash
 
 tmp="$(pwd)/tmp"
-cd "$tmp"
 
 generate_tex(){
+cd "$tmp"
 resume=`ls Resume-*.pdf`
 mkdir -pv html tex
 cd html
-ln -v -s ../$resume resume.pdf
+[[ -L resume.pdf ]] || ln -v -s "$tmp/$resume" resume.pdf
+echo > resume-html.html
 pdftohtml -s resume.pdf -nomerge
-pandoc resume-html.html -o ../tex/resume.tex
-cd ..
+pandoc resume-html.html -o "$tmp/tex/resume.tex"
+}
+
+
+generate_info(){
+cd "$tmp/tex"
+sed '/ - page~\|\hypertarget{page\|\includegraphics\[\|^$/d' resume.tex > info.tex
+sed -i ':x /~~ • ~/ { N; s/\n//g ; bx }' info.tex
+sed -i  '/~~ • ~/ { s/^/\{/g ; s/[ ]\?$/}/g ; s/~~ •[ ]\?~[ ]\?/}{/g }' info.tex
+sed -i 's/ (. months)$//g' info.tex
+}
+
+
+generate_files(){
+cd "$tmp/tex"
+csplit -z info.tex '/\textbf{Summary}\|\textbf{Experience}\|\textbf{Skills}/' '{*}'
+sed -i ':a;N;/\n\\text/! s/\n/}{/;ta;P;D' xx02
+sed -i 's/}}/}/' xx02
+sed -i  '/}$/!  s/$/}/g ' xx02
+mkdir -p "$tmp/translated"
+mkdir -p "$tmp/translated/en"
+sed -n '1p' xx00 > "$tmp/translated/name.tex"
+sed -n '1!p' xx00 > "$tmp/translated/contact.tex"
+sed -n '1!p' xx01 > "$tmp/translated/en/about.tex"
+sed -n '1!p' xx03 > "$tmp/translated/en/skills.tex"
+csplit -z xx02 '/\textbf{Experience}\|\textbf{Education}/' '{*}'
+sed -i -n '1!p' xx00 
+sed -i -n '1!p' xx01 
+nl -w 3 -s == xx00 > "$tmp/translated/en/experience.tex"
+nl -w 3 -v 3 -s == xx01 > "$tmp/translated/en/education.tex"
+}
+
+
+process_files(){
+cd "$tmp/translated"
+
+
 }
 
 # generate_tex
-
-
-cd "$tmp/tex"
-
-sed '/ - page~/d' resume.tex > a.tex
-sed -i '/\hypertarget{page/d' a.tex
-sed -i '/\includegraphics\[/d' a.tex
-sed -i  '/^/N;s/\n//' a.tex
-sed -i  '/~~ • ~/N;s/\n/ /' a.tex
-sed -i  '/~~ • ~/N;s/\n/ /' a.tex
-sed -i  '/^$/N;s/\n//' a.tex
-sed -i  '/~~ • ~/ s/^/{/g' a.tex
-sed -i  '/~~ • ~/ s/[ ]\?$/}/g' a.tex
-sed -i 's/~~ •[ ]\?~[ ]\?/}{/g' a.tex
-sed -i 's/ (. months)$//g' a.tex
-
+# generate_info
+generate_files
+# process_files
